1bb339aff24e51b7e981607556a9e5c5c50b0df1
Neuroplant <m.roych@gmail.com>
2019-03-19 05:25:14 +0100

    Update ServoCheck.c

--- ServoCheck.c
+++ ServoCheck.c
@@ -187,13 +187,13 @@ int main (int argc, char *argv[]) {
 	fprintf(fp,"\n// Modul: %i\n",PIN_BASE0);
 		for (i=0;i<16;i++) {
 			if (Servo[i].num<30) {
-				fprintf(fp,"\n Servo[%i].pin = %i + PIN_BASE0; \n Servo[%i].min = %i; \n Servo[%i].max = %i; \n Servo[%i].neu = %i; \n",Servo[i].num,i,Servo[i].num,Servo[i].min,Servo[i].num,Servo[i].max,Servo[i].num,((Servo[i].min+Servo[i].max)/2));
+				fprintf(fp,"\n Servo[%i].pin = %i + PIN_BASE0; \n Servo[%i].min = %i; \n Servo[%i].max = %i; \n Servo[%i].neu = %i; \nServo[%i].alt = %i; \n",Servo[i].num,i,Servo[i].num,Servo[i].min,Servo[i].num,Servo[i].max,Servo[i].num,((Servo[i].min+Servo[i].max)/2),Servo[i].num,((Servo[i].min+Servo[i].max)/2));
 			}
 		}
 		fprintf(fp,"\n// Modul: %i\n",PIN_BASE1);
 		for (i=17;i<34;i++) {
 			if (Servo[i].num<30) {
-				fprintf(fp,"\n Servo[%i].pin = %i + PIN_BASE1; \n Servo[%i].min = %i; \n Servo[%i].max = %i; \n Servo[%i].neu = %i; \n",Servo[i].num,(i-17),Servo[i].num,Servo[i].min,Servo[i].num,Servo[i].max,Servo[i].num,((Servo[i].min+Servo[i].max)/2));
+				fprintf(fp,"\n Servo[%i].pin = %i + PIN_BASE1; \n Servo[%i].min = %i; \n Servo[%i].max = %i; \n Servo[%i].neu = %i; \nServo[%i].alt = %i; \n",Servo[i].num,(i-17),Servo[i].num,Servo[i].min,Servo[i].num,Servo[i].max,Servo[i].num,((Servo[i].min+Servo[i].max)/2),Servo[i].num,((Servo[i].min+Servo[i].max)/2));
 			}
 		}
 	fclose(fp);       
